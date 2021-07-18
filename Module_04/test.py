eeba09266f420cb87a80abf9923255ae69c9f491
Snigdha Kundu <roy.sniigdha@gmail.com>
2021-07-18 18:10:01 -0400

    module 4 : test start

---
+++ Module_04/test.py
@@ -0,0 +1,30 @@
+# Dependencies and Setup
+import pandas as pd
+import numpy as np 
+
+
+# File to Load (Remember to change the path if needed.)
+school_data_to_load = "Resources/schools_complete.csv"
+student_data_to_load = "Resources/students_complete.csv"
+
+# Read the School Data and Student Data and store into a Pandas DataFrame
+school_data_df = pd.read_csv(school_data_to_load)
+student_data_df = pd.read_csv(student_data_to_load)
+
+# Cleaning Student Names and Replacing Substrings in a Python 
+
+# Add each prefix and suffix to remove to a list.
+prefixes_suffixes = ["Dr. ", "Mr. ","Ms. ", "Mrs. ", "Miss ", " MD", " DDS", " DVM", " PhD"]
+
+# Iterate through the words in the "prefixes_suffixes" list and replace them with an empty space, "".
+for word in prefixes_suffixes:
+    student_data_df["student_name"] = student_data_df["student_name"].str.replace(word,"")
+
+# Check names.
+student_data_df.head(10)
+#print(student_data_df.head(10))
+
+student_data_df.loc[student_data_df["reading_score"]>=70 ]
+
+print( student_data_df.loc[student_data_df["reading_score"]>=70 ])
+# missing_grade_df.dropna()
